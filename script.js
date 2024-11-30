e03a6799533122b0200400c7977ae5308a5f7885
SnrKarambite <daniel.nad.4020@gmail.com>
2024-11-30 12:22:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    function toggleMenu() {
+        const menu = document.querySelector('.nav-menu');
+        const hamburger = document.querySelector('.hamburger');
+        menu.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    }
+</script>
