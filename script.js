730e9c211973e2a02a4240507e42bb5d6e2bfbdf
SnrKarambite <daniel.nad.4020@gmail.com>
2024-11-30 14:09:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-<script>
-    function toggleMenu() {
-        const menu = document.querySelector('.nav-menu');
-        const hamburger = document.querySelector('.hamburger');
-        menu.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    }
-</script>
+// Функция для переключения состояния меню
+function toggleMenu() {
+    const menu = document.querySelector('.nav-menu');
+    const hamburger = document.querySelector('.hamburger');
+    
+    // Переключение активных классов
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
+
+// Добавление обработчика события для клика на гамбургер
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', toggleMenu);
